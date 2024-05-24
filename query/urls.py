bc2bce9d4568a64e8583d328bbfe566fd1fcc57d
John Blalock <John.Blalock@officeally.com>
2024-05-24 15:35:52 -0700

    first service working update

--- query/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('alllogsets/', views.list_all_logsets, name='list_all_logsets'),
-    path('', views.query_home, name='query_home'),
-    path('search_logs/', views.search_logs, name='search_logs'),
-    # Other URL patterns...
-]
